693682514447e22470a787c9129ee534dfaa80a7
PatrykFlama <patrykflama@interia.pl>
2023-06-09 20:06:51 +0200

    2021IIAGIMROZ setup

--- gen_names.sh
+++ gen_names.sh
@@ -7,4 +7,15 @@ do
     if [ $NUM == 1 ]; then
         echo "${p/$'\r'/}";     # remove carriage return
     fi;
-done
+done < names.txt > temp.txt
+
+rm names.txt
+mv temp.txt names.txt
+
+# further processing required
+# while read p; do 
+#     mkdir $p;
+#     touch $p/main.cpp;
+#     touch $p/README.md;
+#     echo "* [$p](XXX/$p)";
+# done < names.txt
